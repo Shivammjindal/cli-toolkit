1648225a084034f37cb44ddc9a4f15ad51da8911
Shivam Jindal <shivamjindals2002@gmail.com>
2026-03-08 18:38:06 +0530

    done

--- vault_cli_linux.sh
+++ vault_cli_linux.sh
@@ -2,5 +2,5 @@ apt update -y
 apt install python3 -y
 apt install python3-pip -y
 wget https://github.com/Shivammjindal/cli-toolkit/raw/refs/heads/main/mycompany_cli-1.0.0-py3-none-any.whl
-pip3 install mycompany_cli-1.0.0-py3-none-any.whl
+pip3 install mycompany_cli-1.0.0-py3-none-any.whl --break-system-packages
 rm mycompany_cli-1.0.0-py3-none-any.whl
